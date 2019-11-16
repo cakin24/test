2bdd7a914121d948f18a7e91ccfa596f05c0b001
cakin24 <798103175@qq.com>
2019-11-16 16:49:50 +0800

    Initial commit

---
+++ src/main/java/ExectorThread.java
@@ -0,0 +1,6 @@
+public class ExectorThread implements Runnable{
+    public void run(){
+        LazySimpleSingleton singleton = LazySimpleSingleton.getInstance();
+        System.out.println(Thread.currentThread().getName()+":"+singleton);
+    }
+}
---
+++ src/main/java/LazySimpletonTest.java
@@ -0,0 +1,10 @@
+public class LazySimpletonTest {
+    public static void main(String[] args)
+    {
+        Thread t1 = new Thread(new ExectorThread());
+        Thread t2 = new Thread(new ExectorThread());
+        t1.start();
+        t2.start();
+        System.out.println("END");
+    }
+}

a390f0807e7f710eff5a998ed100a73db62f8120
cakin24 <798103175@qq.com>
2019-12-21 20:41:32 +0800

    Jad反编译

--- src/main/java/DataDynamicProxy/OrderService.java
+++ src/main/java/DataDynamicProxy/OrderService.java
@@ -7,11 +7,8 @@ public class OrderService implements IOrderService {
         orderDao = new OrderDao();
     }
 
-
     public int createOrder(Order order){
         System.out.println("orderService调用orderDao创建订单");
         return orderDao.insert(order);
     }
-
-
 }
